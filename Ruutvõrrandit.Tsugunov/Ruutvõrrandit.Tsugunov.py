719492c8abe59e622925ecb9734154181a199b5d
TsugunovS <stanislav.tsugunov@gmail.com>
2023-03-20 12:11:56 +0200

    RuutV

--- Ruutvõrrandit.Tsugunov/Ruutvõrrandit.Tsugunov.py
+++ Ruutvõrrandit.Tsugunov/Ruutvõrrandit.Tsugunov.py
@@ -7,43 +7,94 @@ def text_to_lbl(event):
 aken=Tk()
 aken.geometry("650x260")
 aken.title("Ruutvõrrandid")
+aken.iconbitmap("xterm.ico")
 
-pealkiri=Label(aken,
-               text="Ruutvõrrandid",
+pealkiri=Label(aken, #1
+               text="Решение квадратного уравнения",
                bg="lightblue",
-               fg="#32a852",
+               fg="#168c35",
                font="Times_new_roma 20",
                height=2,
                width=28)
 pealkiri.pack(pady=20)
 
-tekst_kast=Entry(aken,
+nupp2=Button(aken, #1
+               text="Решение",
+               bg="Yellow",
+               fg="black",
+               font="Times_new_roma 14",
+               activebackground="lightblue",
+               height=2,
+               width=40)
+pealkiri.pack(pady=20)
+
+tekst_kast=Entry(aken, #2
                  fg="#24ab67",
                  bg="Lightblue",
                  font="Algerian 20",
                  width=3,
                  justify=LEFT)
 
-nupp=Button(aken,
+pealkiri2=Label(aken, #3
+               text="x**2",
+               fg="#168c35",
+               font="Times_new_roma 20",
+               height=5,
+               width=4,
+               justify=LEFT)
+
+tekst_kast2=Entry(aken, #4
+                 fg="#24ab67",
+                 bg="Lightblue",
+                 font="Algerian 20",
+                 width=3,
+                 justify=LEFT)
+
+pealkiri3=Label(aken, #5
+               text="x+",
+               fg="#168c35",
+               font="Times_new_roma 20",
+               height=5,
+               width=2,
+               justify=LEFT)
+
+tekst_kast3=Entry(aken, #6
+                 fg="#24ab67",
+                 bg="Lightblue",
+                 font="Algerian 20",
+                 width=3,
+                 justify=LEFT)
+
+pealkiri4=Label(aken, #7
+               text="=0",
+               fg="#168c35",
+               font="Times_new_roma 20",
+               activebackground="#a80a1a",
+               height=5,
+               width=2,
+               justify=LEFT)
+
+nupp=Button(aken, #8
             text="Решить",
-            bg="lightblue",
+            bg="Green",
             fg="#020812",
-            font="Times_new_roma 20",
+            font="Algerian 20",
             activebackground="lightblue",
             height=2,
-            width=24,
-            anchor=E)
+            width=7,
+            justify=LEFT)
 
-tekst_kast=Entry(aken,
-                 fg="lightblue",
-                 bg="grey",
-                 font="Algerian 20",
-                 width=20,
-                 justify=CENTER)
-tekst_kast.bind("<Return>", text_to_lbl)
 
-tekst_kast.pack()
-tekst_kast.pack(side=LEFT)
-pealkiri.pack()
-nupp.pack()
+
+
+nupp2.pack(side=BOTTOM)
+pealkiri.pack() #1
+tekst_kast.pack(side=LEFT) #2
+pealkiri2.pack(side=LEFT) #3
+tekst_kast2.pack(side=LEFT) #4
+pealkiri3.pack(side=LEFT) #5
+tekst_kast3.pack(side=LEFT) #6
+pealkiri4.pack(side=LEFT) #7
+nupp.pack(side=LEFT) #8
+side=BOTTOM
 aken.mainloop()
